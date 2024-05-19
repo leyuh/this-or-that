19a77aabfaa49faf95d49ed8b7f4e2866e39ba5a
Claire Devlin <clairebeargoldielox@icloud.com>
2024-05-18 22:27:39 -0700

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*images.stockcake.com",
+            },
+            {
+                protocol: "https",
+                hostname: "i.abcnewsfe.com*",
+            },
+            {
+                protocol: "https",
+                hostname: "static01.nyt.com*",
+            },
+        ]
+    }
+};
 
 export default nextConfig;
---
+++ src/modules/optionsData.js
@@ -0,0 +1,94 @@
+const OPTIONS_DATA = [
+    {
+        "name": "glasses",
+        "desc": "Only 63.7% of the population need them. Not that important, right?",
+        "image": "https://images.stockcake.com/public/b/c/d/bcdee869-c075-4ef1-a083-dbd26ff11235_large/stylish-aviator-glasses-stockcake.jpg"
+    },
+    {
+        "name": "Azerbaijan",
+        "desc": "0.125% of the world's population.",
+        "image": "https://images.stockcake.com/public/8/f/0/8f0e08d3-a9a8-470b-b315-37a846a8570f_large/colorful-market-display-stockcake.jpg"
+    },
+    {
+        "name": "your first crush",
+        "desc": "When I was 13, I had my first love...",
+        "image": "https://images.stockcake.com/public/5/2/a/52af2785-792b-43d4-9681-afd5a29839cc_large/love-hearts-decoration-stockcake.jpg"
+    },
+    {
+        "name": "Robert Downey, Jr.",
+        "desc": "",
+        "image": "https://i.abcnewsfe.com/a/fd751005-a167-478b-bba2-ccaf61e26bf6/iron-man-rdjr-1-ht-thg-231204_1701712078177_hpMain.jpg"
+    },
+    {
+        "name": "nail clippers",
+        "desc": "Meh, you have scissors, you don't need them, right?",
+        "image": "https://images.stockcake.com/public/5/f/5/5f5576e9-1c82-4ec8-9159-f2a93d36050d_large/nail-clipping-time-stockcake.jpg"
+    },
+    {
+        "name": "the Super Bowl",
+        "desc": "An American tradition.",
+        "image": "https://images.stockcake.com/public/4/8/4/484f1333-fb81-47fe-bf12-47bd6bd2f542_large/intense-football-action-stockcake.jpg"
+    },
+    {
+        "name": "Wikipedia",
+        "desc": "If you are an English teacher, you definitely won't pick this.",
+        "image": "https://images.stockcake.com/public/e/2/5/e2544675-fdb3-435e-b8a6-959f14e28bfa_large/elderly-man-computing-stockcake.jpg"
+    },
+    {
+        "name": "the moon",
+        "desc": "It's just a big rock.",
+        "image": "https://images.stockcake.com/public/4/e/a/4ea73628-1864-4916-9969-1a56ea9723d8_large/luminous-moon-night-stockcake.jpg"
+    },
+    {
+        "name": "turtles",
+        "desc": "They don't do anything. It's fine.",
+        "image": "https://images.stockcake.com/public/5/6/b/56bee560-41db-47f8-846c-b2ab9d25271f_large/turtle-at-sunset-stockcake.jpg"
+    },
+    {
+        "name": "door knobs",
+        "desc": "You can just kick it down.",
+        "image": "https://images.stockcake.com/public/2/1/0/21088e48-5a3b-437d-b2a0-c52af4797aab_large/vintage-door-knob-stockcake.jpg"
+    },
+    {
+        "name": "tiramisu",
+        "desc": "An Italian staple.",
+        "image": "https://images.stockcake.com/public/1/a/3/1a3d2661-3884-4bd8-a44c-7a8f446077ac_large/decadent-tiramisu-dessert-stockcake.jpg"
+    },
+    {
+        "name": "gingers",
+        "desc": "Not sure why anyone would keep them tbh.",
+        "image": "https://images.stockcake.com/public/7/3/f/73fc4571-5dff-476d-bc15-8e200fed6d3d_large/radiant-ginger-smile-stockcake.jpg"
+    },
+    {
+        "name": "jazz",
+        "desc": "ya like jazz?",
+        "image": "https://images.stockcake.com/public/5/1/8/518e4299-c370-409a-b1e2-4273568e9021_large/jazz-night-live-stockcake.jpg"
+    },
+    {
+        "name": "denim",
+        "desc": "No one likes jeans anyway.",
+        "image": "https://images.stockcake.com/public/a/a/5/aa5e7aea-a1a1-4b3b-bf92-f05c64b2cab6_large/folded-denim-jeans-stockcake.jpg"
+    },
+    {
+        "name": "seafoam green",
+        "desc": "It's just another shade of green.",
+        "image": "https://images.stockcake.com/public/0/8/9/089e16b0-c667-48e8-a27e-7c664a4da455_large/intricate-seafoam-structure-stockcake.jpg"
+    },
+    {
+        "name": "Colorado",
+        "desc": "There's still 49 other states...",
+        "image": "https://images.stockcake.com/public/b/b/4/bb4cea79-0b8b-4d44-962f-ed406eaf2757_large/stunning-canyon-view-stockcake.jpg"
+    },
+    {
+        "name": "Billie Eilish",
+        "desc": "GLOCK TUCKED BIG T-SHIRT BILLIE EILISH",
+        "image": "https://static01.nyt.com/images/2020/03/15/magazine/15mag-billie-03/15mag-billie-03-mediumSquareAt3X-v3.jpg"
+    },
+    {
+        "name": "NASA",
+        "desc": "I guess you'd probably save lives if you got rid of this.",
+        "image": "https://images.stockcake.com/public/8/0/5/8058e1d9-b5b2-447b-8206-ec206fce8f39_large/rocket-liftoff-flames-stockcake.jpg"
+    },
+];
+
+export default OPTIONS_DATA;
